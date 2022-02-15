ea1534ed3e198dbf788bf7820e5c900756800dc8
Hyup <kkddong98@gmail.com>
2022-02-16 00:16:00 +0900

    no message

--- src/Game/Character.java
+++ src/Game/Character.java
@@ -1,4 +1,91 @@
 package Game;
 
 public class Character {
+    //ABOUT ITEM//
+    private int magazieItemCount;
+    private int powerItemCount;
+    private int speedItemCount;
+    private int healthItemCount;
+    private int bulletItemCount;
+
+    //ABOUT CHARACTER//
+    private int healthPoint;
+    private int bulletCount;
+    private double x;
+    private double y;
+
+    public Character() {
+
+    }
+
+    public void reload() {
+
+    }
+
+    public void shoot() {
+
+    }
+
+    //동
+    public void moveE() {
+
+    }
+
+    //서
+    public void moveW() {
+
+    }
+
+    //남
+    public void moveS() {
+
+    }
+
+    //북
+    public  void moveN() {
+
+    }
+
+    //북서
+    public void moveNW() {
+
+    }
+
+    //남동
+    public void moveSE() {
+
+    }
+
+    //북동
+    public void moveNE() {
+
+    }
+
+    //남서
+    public void moveSW() {
+
+    }
+
+    void getItem(Item item) {
+        switch (item) {
+            case POWER:
+                break;
+            case SPEED:
+                break;
+            case BULLET:
+                break;
+            case HEALTH:
+                break;
+            case MAGAZINE:
+                break;
+        }
+    }
+
+    public void getdamage(int damage) {
+
+    }
+
+
+
+
 }
--- src/Game/Item.java
+++ src/Game/Item.java
@@ -1,4 +1,9 @@
 package Game;
 
 public enum Item {
+    MAGAZINE,
+    POWER,
+    SPEED,
+    HEALTH,
+    BULLET,
 }
